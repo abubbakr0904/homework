cd8f7e02774ca28fffd3f6f3ca656a5c459625c9
abubbakr0904 <abubbakroqilov03@gmail.com>
2024-04-30 10:34:11 +0300

    good

---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(
+          Icons.add,
+          color : Colors.red
+        ),
+        title: Text("Assalom Otabek"),
+      ),
+    );
+  }
+}
